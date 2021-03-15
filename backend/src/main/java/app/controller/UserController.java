585b536355bae936338d8eca853c2e894c4b0c17
jovan-jelicki <jovanzte@gmail.com>
2021-03-15 15:22:16 +0100

    backend package

--- backend/src/main/java/app/controller/UserController.java
+++ backend/src/main/java/app/controller/UserController.java
@@ -2,7 +2,6 @@ package app.controller;
 
 import app.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- backend/src/main/java/app/service/UserService.java
+++ backend/src/main/java/app/service/UserService.java
@@ -1,7 +1,6 @@
 package app.service;
 
 import app.model.User;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserService extends CRUDService<User>{
     User findByEmail(String email);
