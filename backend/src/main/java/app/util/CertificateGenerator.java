b88c0fc4f0242524f32add6580d430351cdbbeeb
Tamara <t.kovacevic98@gmail.com>
2021-03-23 15:14:21 +0100

    set key usages

--- backend/src/main/java/app/util/CertificateGenerator.java
+++ backend/src/main/java/app/util/CertificateGenerator.java
@@ -28,29 +28,21 @@ import java.util.Collections;
 public class CertificateGenerator {
     public X509Certificate generateCertificate(SubjectData subjectData, IssuerData issuerData, PrivateKey privateKey) {
         try {
-            //Posto klasa za generisanje sertifiakta ne moze da primi direktno privatni kljuc pravi se builder za objekat
-            //Ovaj objekat sadrzi privatni kljuc izdavaoca sertifikata i koristiti se za potpisivanje sertifikata
-            //Parametar koji se prosledjuje je algoritam koji se koristi za potpisivanje sertifiakta
             JcaContentSignerBuilder builder = new JcaContentSignerBuilder("SHA256WithRSAEncryption");
-            //Takodje se navodi koji provider se koristi, u ovom slucaju Bouncy Castle
             builder = builder.setProvider("BC");
 
-            //Formira se objekat koji ce sadrzati privatni kljuc i koji ce se koristiti za potpisivanje sertifikata
             ContentSigner contentSigner;
             if(issuerData.getPrivateKey() == null)
                 contentSigner = builder.build(privateKey);
             else
                 contentSigner = builder.build(issuerData.getPrivateKey());
 
-            //Postavljaju se podaci za generisanje sertifiakta
             X509v3CertificateBuilder certGen = new JcaX509v3CertificateBuilder(issuerData.getX500name(),
                     new BigInteger(subjectData.getSerialNumber()),
                     subjectData.getStartDate(),
                     subjectData.getEndDate(),
                     subjectData.getX500name(),
                     subjectData.getPublicKey());
-            //Generise se sertifikat
-        //    KeyUsage usage = new KeyUsage(KeyUsage.keyCertSign | KeyUsage.digitalSignature | KeyUsage.keyEncipherment | KeyUsage.dataEncipherment | KeyUsage.cRLSign);
             ArrayList<Boolean> booleans = new ArrayList<Boolean>(Arrays.asList(false, true, true, false,true, true,false,true,false)) {};
             int sum = ExtensionsUtil.convertKeyUsageArrayToInt(booleans);
             KeyUsage usage = new KeyUsage(sum);
@@ -58,12 +50,9 @@ public class CertificateGenerator {
 
             X509CertificateHolder certHolder = certGen.build(contentSigner);
 
-            //Builder generise sertifikat kao objekat klase X509CertificateHolder
-            //Nakon toga je potrebno certHolder konvertovati u sertifikat, za sta se koristi certConverter
             JcaX509CertificateConverter certConverter = new JcaX509CertificateConverter();
             certConverter = certConverter.setProvider("BC");
 
-            //Konvertuje objekat u sertifikat
             return certConverter.getCertificate(certHolder);
         } catch (IllegalArgumentException | IllegalStateException | OperatorCreationException | CertificateException e) {
             e.printStackTrace();
