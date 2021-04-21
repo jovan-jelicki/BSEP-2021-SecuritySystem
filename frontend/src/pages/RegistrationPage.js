df2e6b932a86baf6daa669c543c9168c00f48aed
jovan-jelicki <jovanzte@gmail.com>
2021-04-21 18:37:26 +0200

    Login page reCaptcha - Registration page with validation

--- frontend/src/pages/RegistrationPage.js
+++ frontend/src/pages/RegistrationPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PasswordStrengthBar from 'react-password-strength-bar';
 import {Alert, Button, FormControl} from "react-bootstrap";
 import axios from "axios";
 
@@ -6,22 +7,18 @@ export default class RegistrationPage extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            user: {
-                id:'',
-                email: '',
-                password: '',
-                firstName: '',
-                lastName: '',
-            },
-            errors:{
-                user: {
-                    'email': 'Enter email',
-                    'password': 'Enter password',
-                    'firstName': 'Enter First name',
-                    'lastName': 'Enter Last name',
-                    'rePassword' : 'Repeat password'
-                }
-            },
+            passwordStrength : "",
+            id:'',
+            email: '',
+            password: '',
+            firstName: '',
+            lastName: '',
+            rePassword : '',
+            emailErr: 'Enter email',
+            passwordErr: 'Enter password',
+            firstNameErr: 'Enter First name',
+            lastNameErr: 'Enter Last name',
+            rePasswordErr: 'Repeat password',
             validForm: false,
             submitted: false,
             successfullyReg:false,
@@ -31,23 +28,18 @@ export default class RegistrationPage extends React.Component{
     }
 
     handleInputChange = (event) => {
-        //console.log(event.target.value)
-        const { name, value } = event.target;
-        const user = this.state.user;
-        user[name] = value;
-
-        this.setState({ user });
-       // console.log(this.state.user.password)
-
+        const target = event.target;
+        this.setState({
+            [target.name] : target.value,
+        })
         this.validationErrorMessage(event);
     }
 
     handlePassChange = (event) => {
-       // console.log("dosao")
-       // console.log(event.target.value)
-        this.state.rePassword=event.target.value;
-        //console.log(this.state.rePassword)
-        this.validationErrorMessage(event);
+        this.setState(
+            (state,props) => ({ rePassword : event.target.value}),
+            () => this.validationErrorMessage(event)
+        )
     }
 
     validationErrorMessage = (event) => {
@@ -56,31 +48,57 @@ export default class RegistrationPage extends React.Component{
 
         switch (name) {
             case 'firstName':
-                errors.user.firstName = value.length < 1 ? 'Enter First Name' : '';
+                this.setState({
+                    firstNameErr : this.state.firstName.length < 1 ? 'EnterFirstName' : ''
+                })
                 break;
             case 'lastName':
-                errors.user.lastName = value.length < 1 ? 'Enter Last Name' : '';
+                this.setState({
+                    lastNameErr : this.state.lastName.length < 1 ? 'EnterLastName' : ''
+                })
                 break;
             case 'email':
-                errors.user.email = this.isValidEmail(value) ? '' : 'Email is not valid!';
+                this.setState({
+                    emailErr : this.isValidEmail(this.state.email) && this.state.email.length > 1 ? '' : 'Email is not valid!'
+                })
                 break;
             case 'password':
-                errors.user.password = value.length < 1 ? 'Enter Password' : '';
+                this.setState({
+                    passwordErr : this.checkPassword(this.state.password) ? 'Password must contains at least 8 characters (lowercase letter, capital letter, number and special character)' : ''
+                })
                 break;
             case 'rePassword':
-                errors.user.rePassword = this.isValidPassword(value) ? '' : 'This password must match the previous';
+                this.setState({
+                    rePasswordErr : this.isValidRepeatedPassword(this.state.rePassword) ? '' : 'This password must match the previous'
+                })
                 break;
             default:
                 break;
         }
 
-        this.setState({ errors });
     }
+
+    checkPassword =  (password) =>{
+        if(/^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{8,}$/.test(password)){
+            this.setState({
+                passwordStrength: this.state.password
+            })
+            return false;
+        }else {
+            this.setState({
+                passwordStrength : ""
+            })
+            return true;
+        }
+
+    }
+
     isValidEmail = (value) => {
-        return !(value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,64}$/i.test(value))
+        var Proba = !(value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,64}$/i.test(value))
+        return Proba;
     }
-    isValidPassword = (value) => {
-        if(this.state.user.password !== this.state.rePassword) {
+    isValidRepeatedPassword = (value) => {
+        if(this.state.password !== this.state.rePassword) {
             return false;
         }else{
             return  true
@@ -89,12 +107,10 @@ export default class RegistrationPage extends React.Component{
 
     submitForm = async (event) => {
         this.setState({submitted: true});
-        const user = this.state.user;
         event.preventDefault();
-        if (this.validateForm(this.state.errors)) {
-            console.info('Valid Form')
-            console.log(this.state.user)
-            this.sendParams()
+        const Errors = ['email', 'password', 'firstName', 'rePassword', 'lastName'];
+        if (this.validateForm(Errors)) {
+            await this.sendParams()
         } else {
             console.log('Invalid Form')
         }
@@ -102,21 +118,24 @@ export default class RegistrationPage extends React.Component{
 
     validateForm = (errors) => {
         let valid = true;
-        Object.entries(errors.user).forEach(item => {
-            //console.log(item)
-            item && item[1].length > 0 && (valid = false)
-        })
+        for(const Error of errors) {
+            this.validationErrorMessage(this.createTarget(Error));
+        }
         return valid;
+    }
+
+    createTarget = (error) => {
+        return {target : {value : error, name : error}}
     }
 
     async sendParams() {
         axios
             .post('http://localhost:8080/auth/save', {
                 'id':'',
-                'firstName' : this.state.user.firstName,
-                'lastName' : this.state.user.lastName,
-                'email' : this.state.user.email,
-                'password' : this.state.user.password,
+                'firstName' : this.state.firstName,
+                'lastName' : this.state.lastName,
+                'email' : this.state.email,
+                'password' : this.state.password,
             })
             .then(res => {
                 this.setState({ errorMessage:false });
@@ -126,7 +145,6 @@ export default class RegistrationPage extends React.Component{
             this.setState({ errorMessage:true });
         })
 
-        ;
     }
 
     render() {
@@ -136,16 +154,16 @@ export default class RegistrationPage extends React.Component{
                 <div className="row">
                     <label className="col-sm-2 col-form-label">Name</label>
                     <div className="col-sm-5 mb-2">
-                        <input  disabled = {(this.state.disabled)? "disabled" : ""} type="text" value={this.state.user.firstName} name="firstName" onChange={(e) => {
+                        <input  disabled = {(this.state.disabled)? "disabled" : ""} type="text" value={this.state.firstName} name="firstName" onChange={(e) => {
                             this.handleInputChange(e)
                         }} className="form-control" placeholder="First Name"/>
-                        {this.state.submitted && this.state.errors.user.firstName.length > 0 &&
-                        <span className="text-danger">{this.state.errors.user.firstName}</span>}
+                        {this.state.submitted && this.state.firstNameErr.length > 0 &&
+                        <span className="text-danger">{this.state.firstNameErr}</span>}
 
                     </div>
                     <div className="col-sm-5 mb-2">
                         <input  disabled = {(this.state.disabled)? "disabled" : ""}  type="text" value={this.state.lastName} name="lastName" onChange={(e) => {this.handleInputChange(e) }} className="form-control" placeholder="Last Name"/>
-                        {this.state.submitted && this.state.errors.user.lastName.length > 0 && <span className="text-danger">{this.state.errors.user.lastName}</span>}
+                        {this.state.submitted && this.state.lastNameErr.length > 0 && <span className="text-danger">{this.state.lastNameErr}</span>}
 
                     </div>
                     <div className="col-sm-4">
@@ -154,8 +172,8 @@ export default class RegistrationPage extends React.Component{
                 <div className="row"style={{marginTop: '1rem'}}>
                     <label  className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-6 mb-2">
-                        <input  disabled = {(this.state.disabled)? "disabled" : ""}   type="email" value={this.state.user.email} name="email" onChange={(e) => {this.handleInputChange(e)}}className="form-control" id="email" placeholder="example@gmail.com" />
-                        {this.state.submitted && this.state.errors.user.email.length > 0 && <span className="text-danger">{this.state.errors.user.email}</span>}
+                        <input  disabled = {(this.state.disabled)? "disabled" : ""}   type="email" value={this.state.email} name="email" onChange={(e) => {this.handleInputChange(e)}}className="form-control" id="email" placeholder="example@gmail.com" />
+                        {this.state.submitted && this.state.emailErr.length > 0 && <span className="text-danger">{this.state.emailErr}</span>}
 
                     </div>
                     <div className="col-sm-4">
@@ -164,9 +182,9 @@ export default class RegistrationPage extends React.Component{
                 <div className="row"style={{marginTop: '1rem'}}>
                     <label className="col-sm-2 col-form-label">Password</label>
                     <div className="col-sm-6 mb-2">
-                        <FormControl  disabled = {(this.state.disabled)? "disabled" : ""}  name="password" type="password" placeholder="Password"  value={this.state.user.password} onChange={(e) => {this.handleInputChange(e)}}/>
-                        {this.state.submitted && this.state.errors.user.password.length > 0 &&  <span className="text-danger">{this.state.errors.user.password}</span>}
-
+                        <FormControl disabled = {(this.state.disabled)? "disabled" : ""}  name="password" type="password" placeholder="Password"  value={this.state.password} onChange={(e) => {this.handleInputChange(e)}}/>
+                        {this.state.submitted && this.state.passwordErr.length > 0 &&  <span className="text-danger">{this.state.passwordErr}</span>}
+                        <PasswordStrengthBar password={this.state.passwordStrength} />
                     </div>
                     <div className="col-sm-4">
                     </div>
@@ -176,7 +194,7 @@ export default class RegistrationPage extends React.Component{
                     <label  className="col-sm-2 col-form-label">Repeat password</label>
                     <div className="col-sm-6 mb-2">
                         <FormControl  disabled = {(this.state.disabled)? "disabled" : ""}  name="rePassword" type="password" placeholder="Repeat new Password" value={this.state.rePassword} onChange={(e) => {this.handlePassChange(e)}}/>
-                        {this.state.submitted && this.state.errors.user.rePassword.length > 0 &&  <span className="text-danger">{this.state.errors.user.rePassword}</span>}
+                        {this.state.submitted && this.state.rePasswordErr.length > 0 &&  <span className="text-danger">{this.state.rePasswordErr}</span>}
 
                     </div>
                     <div className="col-sm-4">
@@ -189,20 +207,20 @@ export default class RegistrationPage extends React.Component{
                             Successfully registered please login.
                         </Alert>
                         :
-                    <div className="row" style={{marginTop: '1rem'}}>
-                        <div className="col-sm-5 mb-2">
+                        <div className="row" style={{marginTop: '1rem'}}>
+                            <div className="col-sm-5 mb-2">
+                            </div>
+                            <div className="col-sm-4">
+                                <Button variant="success" onClick={this.submitForm}>Confirm</Button>
+                            </div>
                         </div>
-                        <div className="col-sm-4">
-                            <Button variant="success" onClick={this.submitForm}>Confirm</Button>
-                        </div>
-                    </div>
                 }
 
                 {
                     this.state.errorMessage &&
-                        <Alert variant='danger' show={true}  style={({textAlignVertical: "center", textAlign: "center"})}>
-                            The e-mail address must be unique! Please try again
-                        </Alert>
+                    <Alert variant='danger' show={true}  style={({textAlignVertical: "center", textAlign: "center"})}>
+                        The e-mail address must be unique! Please try again
+                    </Alert>
                 }
             </div>
         );
